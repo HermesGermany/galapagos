5f6fdbb3d4f8b3e69520b117bc9900d390e95cfe
Emre Polat <Emre.Polat@hermesworld.com>
2021-05-17 15:09:48 +0200

    Add unit Tests for ConfluentCloudAuthenticationModule

--- src/test/java/com/hermesworld/ais/galapagos/ccloud/ConfluentCloudAuthenticationModuleTest.java
+++ src/test/java/com/hermesworld/ais/galapagos/ccloud/ConfluentCloudAuthenticationModuleTest.java
@@ -1,70 +1,53 @@
 package com.hermesworld.ais.galapagos.ccloud;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.hermesworld.ais.galapagos.applications.ApplicationMetadata;
 import com.hermesworld.ais.galapagos.ccloud.apiclient.ApiKeyInfo;
 import com.hermesworld.ais.galapagos.ccloud.apiclient.ConfluentApiClient;
+import com.hermesworld.ais.galapagos.ccloud.apiclient.ServiceAccountInfo;
 import com.hermesworld.ais.galapagos.ccloud.auth.ConfluentCloudAuthConfig;
 import com.hermesworld.ais.galapagos.ccloud.auth.ConfluentCloudAuthenticationModule;
-import com.hermesworld.ais.galapagos.kafka.KafkaCluster;
-import com.hermesworld.ais.galapagos.kafka.KafkaClusters;
-import com.hermesworld.ais.galapagos.kafka.auth.CreateAuthenticationResult;
 import com.hermesworld.ais.galapagos.kafka.auth.KafkaAuthenticationModule;
 import com.hermesworld.ais.galapagos.kafka.impl.TopicBasedRepositoryMock;
 import com.hermesworld.ais.galapagos.kafka.util.TopicBasedRepository;
 import org.hamcrest.core.StringContains;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 import reactor.core.publisher.Mono;
 
+import java.time.Instant;
+import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.Properties;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 public class ConfluentCloudAuthenticationModuleTest {
 
-    private KafkaClusters kafkaClusters;
-
-    private final TopicBasedRepository<ApplicationMetadata> applicationMetadataRepository = new TopicBasedRepositoryMock<>();
+    private TopicBasedRepository<ApplicationMetadata> applicationMetadataRepository;
 
     private KafkaAuthenticationModule authenticationModule;
 
     private ConfluentApiClient client;
 
     @BeforeEach
-    public void feedMocks() {
-        kafkaClusters = mock(KafkaClusters.class);
+    public void init() {
 
-        KafkaCluster testCluster = mock(KafkaCluster.class);
-        when(testCluster.getRepository("application-metadata", ApplicationMetadata.class))
-                .thenReturn(applicationMetadataRepository);
+        authenticationModule = new ConfluentCloudAuthenticationModule(new ConfluentCloudAuthConfig());
+        client = mock(ConfluentApiClient.class);
+        applicationMetadataRepository = new TopicBasedRepositoryMock<>();
 
-        when(kafkaClusters.getEnvironment("test")).thenReturn(Optional.of(testCluster));
-
-        CreateAuthenticationResult authResult = new CreateAuthenticationResult(
-                new JSONObject(Map.of("testentry", true)), new byte[] { 1 });
-
-        when(authenticationModule.createApplicationAuthentication(eq("quattro-1"), eq("quattro"), any()))
-                .thenReturn(CompletableFuture.completedFuture(authResult));
-        when(kafkaClusters.getAuthenticationModule("test")).thenReturn(Optional.of(authenticationModule));
     }
 
     @Test
     public void extractKafkaUserNameTest_positive() throws ExecutionException, InterruptedException {
-        authenticationModule = new ConfluentCloudAuthenticationModule(new ConfluentCloudAuthConfig());
 
         ApplicationMetadata app = new ApplicationMetadata();
         app.setApplicationId("quattro-1");
@@ -80,7 +63,6 @@ public class ConfluentCloudAuthenticationModuleTest {
 
     @Test
     public void extractKafkaUserNameTest_negative() throws ExecutionException, InterruptedException {
-        authenticationModule = new ConfluentCloudAuthenticationModule(new ConfluentCloudAuthConfig());
 
         ApplicationMetadata app = new ApplicationMetadata();
         app.setApplicationId("quattro-1");
@@ -112,8 +94,6 @@ public class ConfluentCloudAuthenticationModuleTest {
     @Test
     public void fillCorrectProps_negative() {
 
-        authenticationModule = new ConfluentCloudAuthenticationModule(new ConfluentCloudAuthConfig());
-
         Properties props = new Properties();
         authenticationModule.addRequiredKafkaProperties(props);
 
@@ -123,15 +103,8 @@ public class ConfluentCloudAuthenticationModuleTest {
     }
 
     @Test
-    public void createApplicationAuthenticationTest_createServiceAccForAppThatHasNoAcc() {
-
-    }
-
-    @Test
     public void deleteApplicationAuthenticationTest_positive()
-            throws ExecutionException, InterruptedException, JsonProcessingException {
-
-        client = mock(ConfluentApiClient.class);
+            throws ExecutionException, InterruptedException {
 
         ConfluentCloudAuthConfig config = new ConfluentCloudAuthConfig();
         config.setEnvironmentId("testEnv");
@@ -163,6 +136,209 @@ public class ConfluentCloudAuthenticationModuleTest {
         String auth = app.getAuthenticationJson();
         authenticationModule.deleteApplicationAuthentication(app.getApplicationId(), new JSONObject(auth)).get();
         verify(client).deleteApiKey(apiKey1);
+
+    }
+
+    @Test
+    public void deleteApplicationAuthenticationTest_negative() throws ExecutionException, InterruptedException {
+
+        ConfluentCloudAuthConfig config = new ConfluentCloudAuthConfig();
+        config.setEnvironmentId("testEnv");
+        config.setClusterId("testCluster");
+        authenticationModule = new ConfluentCloudAuthenticationModule(config);
+        ReflectionTestUtils.setField(authenticationModule, "client", client);
+
+        ApiKeyInfo apiKey1 = new ApiKeyInfo();
+        apiKey1.setId(1);
+        apiKey1.setKey("someKey1");
+        apiKey1.setSecret("someSecret1");
+        apiKey1.setUserId(123);
+
+        ApiKeyInfo apiKey2 = new ApiKeyInfo();
+        apiKey2.setId(2);
+        apiKey2.setKey("someKey2");
+        apiKey2.setSecret("someSecret2");
+        apiKey2.setUserId(324);
+
+        ApplicationMetadata app = new ApplicationMetadata();
+        app.setApplicationId("quattro-1");
+        app.setAuthenticationJson("{userId:1234}");
+        applicationMetadataRepository.save(app).get();
+
+        when(client.listApiKeys("testEnv", "testCluster")).thenReturn(Mono.just(List.of(apiKey1, apiKey2)));
+
+        String auth = app.getAuthenticationJson();
+        authenticationModule.deleteApplicationAuthentication(app.getApplicationId(), new JSONObject(auth)).get();
+        verify(client, times(0)).deleteApiKey(any());
+
+    }
+
+    @Test
+    public void createApplicationAuthenticationTest_createServiceAccForAppThatHasNoAcc()
+            throws ExecutionException, InterruptedException {
+
+        ConfluentCloudAuthConfig config = new ConfluentCloudAuthConfig();
+        config.setEnvironmentId("testEnv");
+        config.setClusterId("testCluster");
+        authenticationModule = new ConfluentCloudAuthenticationModule(config);
+        ReflectionTestUtils.setField(authenticationModule, "client", client);
+
+        ApiKeyInfo apiKey1 = new ApiKeyInfo();
+        apiKey1.setId(1);
+        apiKey1.setKey("someKey1");
+        apiKey1.setSecret("someSecret1");
+        apiKey1.setUserId(123);
+
+        ApiKeyInfo apiKey2 = new ApiKeyInfo();
+        apiKey2.setId(2);
+        apiKey2.setKey("someKey2");
+        apiKey2.setSecret("someSecret2");
+        apiKey2.setUserId(324);
+
+        ApiKeyInfo apiKey3 = new ApiKeyInfo();
+        apiKey3.setId(3);
+        apiKey3.setKey("someKey3");
+        apiKey3.setSecret("someSecret3");
+        apiKey3.setUserId(143);
+        apiKey3.setCreated(Instant.now());
+
+        ApplicationMetadata app = new ApplicationMetadata();
+        app.setApplicationId("quattro-1");
+        app.setAuthenticationJson("{userId:1234}");
+        applicationMetadataRepository.save(app).get();
+
+        ServiceAccountInfo testServiceAccount = new ServiceAccountInfo();
+        testServiceAccount.setId(Integer.valueOf(1));
+        testServiceAccount.setEmail("testEmail");
+        testServiceAccount.setServiceName("testName");
+
+        when(client.listApiKeys("testEnv", "testCluster")).thenReturn(Mono.just(List.of(apiKey1, apiKey2)));
+        when(client.listServiceAccounts()).thenReturn(Mono.just(Collections.emptyList()));
+        when(client.isLoggedIn()).thenReturn(true);
+        when(client.createServiceAccount("application-normalizedAppNameTest", "APP_quattro-1"))
+                .thenReturn(Mono.just(testServiceAccount));
+        when(client.createApiKey("testEnv", "testCluster", "Application normalizedAppNameTest", 1))
+                .thenReturn(Mono.just(apiKey3));
+
+        String auth = app.getAuthenticationJson();
+
+        authenticationModule.createApplicationAuthentication("quattro-1", "normalizedAppNameTest", new JSONObject(auth))
+                .get();
+
+        verify(client, times(1)).createServiceAccount("application-normalizedAppNameTest", "APP_quattro-1");
+        verify(client, times(1)).createApiKey("testEnv", "testCluster", "Application normalizedAppNameTest", 1);
+
+    }
+
+    @Test
+    public void createApplicationAuthenticationTest_reuseServiceAccIfExists()
+            throws ExecutionException, InterruptedException {
+
+        ConfluentCloudAuthConfig config = new ConfluentCloudAuthConfig();
+        config.setEnvironmentId("testEnv");
+        config.setClusterId("testCluster");
+        authenticationModule = new ConfluentCloudAuthenticationModule(config);
+        ReflectionTestUtils.setField(authenticationModule, "client", client);
+
+        ApiKeyInfo apiKey1 = new ApiKeyInfo();
+        apiKey1.setId(1);
+        apiKey1.setKey("someKey1");
+        apiKey1.setSecret("someSecret1");
+        apiKey1.setUserId(123);
+
+        ApiKeyInfo apiKey2 = new ApiKeyInfo();
+        apiKey2.setId(2);
+        apiKey2.setKey("someKey2");
+        apiKey2.setSecret("someSecret2");
+        apiKey2.setUserId(324);
+
+        ApiKeyInfo apiKey3 = new ApiKeyInfo();
+        apiKey3.setId(3);
+        apiKey3.setKey("someKey3");
+        apiKey3.setSecret("someSecret3");
+        apiKey3.setUserId(143);
+        apiKey3.setCreated(Instant.now());
+
+        ApplicationMetadata app = new ApplicationMetadata();
+        app.setApplicationId("quattro-1");
+        app.setAuthenticationJson("{userId:1234}");
+        applicationMetadataRepository.save(app).get();
+
+        ServiceAccountInfo testServiceAccount = new ServiceAccountInfo();
+        testServiceAccount.setId(Integer.valueOf(1));
+        testServiceAccount.setEmail("testEmail");
+        testServiceAccount.setServiceName("testName");
+        testServiceAccount.setServiceDescription("APP_quattro-1");
+
+        when(client.listApiKeys("testEnv", "testCluster")).thenReturn(Mono.just(List.of(apiKey1, apiKey2)));
+        when(client.listServiceAccounts()).thenReturn(Mono.just(List.of(testServiceAccount)));
+        when(client.isLoggedIn()).thenReturn(true);
+        when(client.createApiKey("testEnv", "testCluster", "Application normalizedAppNameTest", 1))
+                .thenReturn(Mono.just(apiKey3));
+
+        String auth = app.getAuthenticationJson();
+
+        authenticationModule.createApplicationAuthentication("quattro-1", "normalizedAppNameTest", new JSONObject(auth))
+                .get();
+
+        verify(client, times(0)).createServiceAccount(anyString(), anyString());
+        verify(client, times(1)).createApiKey("testEnv", "testCluster", "Application normalizedAppNameTest", 1);
+
+    }
+
+    @Test
+    public void updateApplicationAuthenticationTest() throws ExecutionException, InterruptedException {
+
+        ConfluentCloudAuthConfig config = new ConfluentCloudAuthConfig();
+        config.setEnvironmentId("testEnv");
+        config.setClusterId("testCluster");
+        authenticationModule = new ConfluentCloudAuthenticationModule(config);
+        ReflectionTestUtils.setField(authenticationModule, "client", client);
+
+        ApiKeyInfo apiKey1 = new ApiKeyInfo();
+        apiKey1.setId(1);
+        apiKey1.setKey("someKey1");
+        apiKey1.setSecret("someSecret1");
+        apiKey1.setUserId(123);
+
+        ApiKeyInfo apiKey2 = new ApiKeyInfo();
+        apiKey2.setId(2);
+        apiKey2.setKey("someKey2");
+        apiKey2.setSecret("someSecret2");
+        apiKey2.setUserId(324);
+
+        ApiKeyInfo apiKey3 = new ApiKeyInfo();
+        apiKey3.setId(3);
+        apiKey3.setKey("someKey3");
+        apiKey3.setSecret("someSecret3");
+        apiKey3.setUserId(143);
+        apiKey3.setCreated(Instant.now());
+
+        ApplicationMetadata app = new ApplicationMetadata();
+        app.setApplicationId("quattro-1");
+        app.setAuthenticationJson("{userId:1234,apiKey:someKey1}");
+        applicationMetadataRepository.save(app).get();
+
+        ServiceAccountInfo testServiceAccount = new ServiceAccountInfo();
+        testServiceAccount.setId(Integer.valueOf(1));
+        testServiceAccount.setEmail("testEmail");
+        testServiceAccount.setServiceName("testName");
+        testServiceAccount.setServiceDescription("APP_quattro-1");
+
+        when(client.listApiKeys("testEnv", "testCluster")).thenReturn(Mono.just(List.of(apiKey1, apiKey2)));
+        when(client.listServiceAccounts()).thenReturn(Mono.just(List.of(testServiceAccount)));
+        when(client.isLoggedIn()).thenReturn(true);
+        when(client.deleteApiKey(apiKey1)).thenReturn(Mono.just(true));
+        when(client.createApiKey("testEnv", "testCluster", "Application normalizedAppNameTest", 1))
+                .thenReturn(Mono.just(apiKey3));
+
+        String auth = app.getAuthenticationJson();
+
+        authenticationModule.updateApplicationAuthentication("quattro-1", "normalizedAppNameTest", new JSONObject(),
+                new JSONObject(auth)).get();
+
+        verify(client).deleteApiKey(apiKey1);
+        verify(client, times(1)).createApiKey("testEnv", "testCluster", "Application normalizedAppNameTest", 1);
 
     }
 
