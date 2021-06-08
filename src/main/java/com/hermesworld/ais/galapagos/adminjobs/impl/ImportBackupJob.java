1ce012d3237aba3781ec8cabf96803d94332f2b9
Emre Polat <Emre.Polat@hermesworld.com>
2021-06-08 16:01:32 +0200

    add new adminjob for importing backup

---
+++ src/main/java/com/hermesworld/ais/galapagos/adminjobs/impl/ImportBackupJob.java
@@ -0,0 +1,159 @@
+package com.hermesworld.ais.galapagos.adminjobs.impl;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hermesworld.ais.galapagos.adminjobs.AdminJob;
+import com.hermesworld.ais.galapagos.kafka.KafkaCluster;
+import com.hermesworld.ais.galapagos.kafka.KafkaClusters;
+import com.hermesworld.ais.galapagos.kafka.util.TopicBasedRepository;
+import com.hermesworld.ais.galapagos.util.HasKey;
+import com.hermesworld.ais.galapagos.util.JsonUtil;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StreamUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+
+/**
+ * Admin job to generate a "tooling" certificate for one of the Kafka Clusters configured for Galapagos. <br>
+ * Such "tooling" certificate is e.g. required for operating the Galapagos LeanIX synchronizer microservice. <br>
+ * The job has two parameters:
+ * <ul>
+ * <li><code>--import.file=<i>&lt;json-file></i> - The name of a file from which the Objects are read and imported into
+ * Galapagos.</li>
+ * <li><code>--clearRepos=<i>&lt;boolean></i> - A boolean value, which indicates, whether all Repositories should be
+ * emptied before performing the import. Set it to true, if Repositories should be emptied, otherwise to false, so the
+ * old Objects in the Repositories will still be there and the new ones are added additionally.</li>
+ * </ul>
+ * 
+ * @author PolatEmr
+ *
+ */
+
+@Component
+public class ImportBackupJob implements AdminJob {
+
+    private final KafkaClusters kafkaClusters;
+
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public ImportBackupJob(KafkaClusters kafkaClusters) {
+        this.kafkaClusters = kafkaClusters;
+        this.objectMapper = JsonUtil.newObjectMapper();
+    }
+
+    @Override
+    public String getJobName() {
+        return "import-backup";
+    }
+
+    @Override
+    public void run(ApplicationArguments allArguments) throws Exception {
+        String jsonFile = Optional.ofNullable(allArguments.getOptionValues("import.file"))
+                .flatMap(ls -> ls.stream().findFirst()).orElse(null);
+
+        Boolean emptyRepos = Optional.ofNullable(allArguments.getOptionValues("clearRepos"))
+                .flatMap(ls -> ls.stream().findFirst().map(Boolean::parseBoolean)).orElse(null);
+
+        if (jsonFile == null) {
+            throw new IllegalArgumentException("Please provide a file using --import.file option");
+        }
+
+        if (emptyRepos == null) {
+            throw new IllegalArgumentException(
+                    "Please provide if existing repos should be cleared before importing backup using --clearRepos option");
+        }
+
+        File f = new File(jsonFile);
+        JSONObject data;
+        try (FileInputStream fis = new FileInputStream(f)) {
+            data = new JSONObject(StreamUtils.copyToString(fis, StandardCharsets.UTF_8));
+        }
+
+        if (emptyRepos) {
+            emptyRepos();
+        }
+
+        System.out.println();
+        System.out.println("========================= Starting Backup Import ========================");
+        System.out.println();
+
+        Iterator<String> envIds = data.keys();
+
+        while (envIds.hasNext()) {
+
+            String envId = envIds.next();
+            KafkaCluster env = kafkaClusters.getEnvironment(envId).orElse(null);
+            if (env == null) {
+                System.err.println("Skipping nonexisting environment " + envId + "...");
+                continue;
+            }
+
+            System.out.println("Importing environment " + envId + "...");
+            importBackup(env, data.getJSONObject(envId));
+        }
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private void importBackup(KafkaCluster env, JSONObject data)
+            throws IOException, ExecutionException, InterruptedException {
+        Iterator<String> topics = data.keys();
+
+        while (topics.hasNext()) {
+            String topic = topics.next();
+
+            System.out.println("Currently importing: " + topic);
+
+            TopicBasedRepository repo = env.getRepositories().stream().filter(r -> topic.equals(r.getTopicName()))
+                    .findFirst().orElse(null);
+
+            if (repo == null) {
+                System.err.println("Skipping nonexisting repo " + topic + "...");
+                continue;
+            }
+
+            Class<?> repoClass = repo.getValueClass();
+
+            JSONObject contents = data.getJSONObject(topic);
+
+            Iterator<String> keys = contents.keys();
+            while (keys.hasNext()) {
+                String key = keys.next();
+
+                JSONObject content = contents.getJSONObject(key);
+
+                HasKey o = (HasKey) objectMapper.readValue(content.toString(), repoClass);
+                repo.save(o).get();
+            }
+        }
+    }
+
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+    private void emptyRepos() {
+        System.out.println();
+        System.out.println("====================== Clearing Repos before Starting Backup ======================");
+        System.out.println();
+
+        kafkaClusters.getEnvironmentIds().forEach(envId -> {
+            KafkaCluster cluster = kafkaClusters.getEnvironment(envId).get();
+            for (TopicBasedRepository topicBasedRepository : cluster.getRepositories()) {
+                for (Object object : topicBasedRepository.getObjects()) {
+                    try {
+                        topicBasedRepository.delete((HasKey) object).get();
+                    }
+                    catch (InterruptedException | ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+    }
+}
---
+++ src/test/java/com/hermesworld/ais/galapagos/adminjobs/impl/ImportBackupJobTest.java
@@ -0,0 +1,116 @@
+package com.hermesworld.ais.galapagos.adminjobs.impl;
+
+import com.hermesworld.ais.galapagos.kafka.KafkaCluster;
+import com.hermesworld.ais.galapagos.kafka.KafkaClusters;
+import com.hermesworld.ais.galapagos.kafka.impl.TopicBasedRepositoryMock;
+import com.hermesworld.ais.galapagos.topics.TopicMetadata;
+import com.hermesworld.ais.galapagos.topics.TopicType;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.ApplicationArguments;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ImportBackupJobTest {
+
+    private ImportBackupJob job;
+
+    private KafkaClusters kafkaClusters;
+
+    private KafkaCluster testCluster;
+
+    private final File testFile = new File("src/test/resources/backup-test.json");
+
+    private TopicBasedRepositoryMock<TopicMetadata> topicRepository;
+
+    @Before
+    public void setUp() {
+        kafkaClusters = mock(KafkaClusters.class);
+        testCluster = mock(KafkaCluster.class);
+        when(testCluster.getId()).thenReturn("prod");
+        when(kafkaClusters.getEnvironment("prod")).thenReturn(Optional.of(testCluster));
+        when(kafkaClusters.getEnvironmentIds()).thenReturn(List.of("prod"));
+        job = new ImportBackupJob(kafkaClusters);
+        topicRepository = new TopicBasedRepositoryMock<>() {
+            @Override
+            public Class<TopicMetadata> getValueClass() {
+                return TopicMetadata.class;
+            }
+
+            @Override
+            public String getTopicName() {
+                return "topics";
+            }
+        };
+    }
+
+    @Test
+    @DisplayName("should import backup from import file")
+    public void importBackupTest() throws Exception {
+        // given
+        ApplicationArguments args = mock(ApplicationArguments.class);
+        // when
+        when(args.getOptionValues("import.file")).thenReturn(Collections.singletonList(testFile.getPath()));
+        when(args.getOptionValues("clearRepos")).thenReturn(Collections.singletonList("false"));
+        when(testCluster.getRepositories()).thenReturn(Collections.singletonList(topicRepository));
+        // then
+        job.run(args);
+
+        assertTrue(topicRepository.getObject("de.hlg.events.sales.my-topic-nice").isPresent());
+
+    }
+
+    @Test
+    @DisplayName("should import backup from import file and old metadata in repos should still be present")
+    public void importBackupWithoutClearingExistingRepos() throws Exception {
+        // given
+        ApplicationArguments args = mock(ApplicationArguments.class);
+        topicRepository.save(buildTopicMetadata()).get();
+        // when
+        when(args.getOptionValues("import.file")).thenReturn(Collections.singletonList(testFile.getPath()));
+        when(args.getOptionValues("clearRepos")).thenReturn(Collections.singletonList("false"));
+        when(testCluster.getRepositories()).thenReturn(Collections.singletonList(topicRepository));
+
+        // then
+        job.run(args);
+        assertTrue(topicRepository.getObject("de.hlg.events.sales.my-topic-nice").isPresent());
+        assertTrue(topicRepository.getObjects().size() == 2);
+
+    }
+
+    @Test
+    @DisplayName("should import backup from import file and old metadata in repos should be not present")
+    public void importBackupWithClearingExistingRepos() throws Exception {
+
+        // given
+        ApplicationArguments args = mock(ApplicationArguments.class);
+        topicRepository.save(buildTopicMetadata()).get();
+        // when
+        when(args.getOptionValues("import.file")).thenReturn(Collections.singletonList(testFile.getPath()));
+        when(args.getOptionValues("clearRepos")).thenReturn(Collections.singletonList("true"));
+        when(testCluster.getRepositories()).thenReturn(Collections.singletonList(topicRepository));
+
+        // then
+        job.run(args);
+        assertTrue(topicRepository.getObject("de.hlg.events.sales.my-topic-nice").isPresent());
+        assertTrue(topicRepository.getObjects().size() == 1);
+
+    }
+
+    private TopicMetadata buildTopicMetadata() {
+        TopicMetadata metadata = new TopicMetadata();
+        metadata.setName("testtopic");
+        metadata.setDescription("Testtopic description");
+        metadata.setOwnerApplicationId("123");
+        metadata.setType(TopicType.EVENTS);
+        return metadata;
+    }
+}
