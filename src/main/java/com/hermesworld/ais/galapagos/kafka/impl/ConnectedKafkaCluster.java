dbdcfc89e9d148456fb30d1620290f7ec6366118
Emre Polat <Emre.Polat@hermesworld.com>
2021-11-23 14:26:28 +0100

    adjust code

--- src/main/java/com/hermesworld/ais/galapagos/kafka/impl/ConnectedKafkaCluster.java
+++ src/main/java/com/hermesworld/ais/galapagos/kafka/impl/ConnectedKafkaCluster.java
@@ -19,7 +19,6 @@ import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.acl.*;
 import org.apache.kafka.common.config.ConfigResource;
 import org.apache.kafka.common.config.ConfigResource.Type;
-import org.apache.kafka.common.config.TopicConfig;
 import org.apache.kafka.common.errors.InterruptException;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.resource.PatternType;
@@ -132,18 +131,10 @@ public class ConnectedKafkaCluster implements KafkaCluster {
 
     @Override
     public CompletableFuture<Void> createTopic(String topicName, TopicCreateParams topicCreateParams) {
-        List<ConfigEntry> configs = topicCreateParams.getTopicConfigs().entrySet().stream()
-                .map(entry -> new ConfigEntry(entry.getKey(), entry.getValue())).collect(Collectors.toList());
-
-        ConfigResource res = new ConfigResource(Type.TOPIC, topicName);
-        Config config = new Config(configs);
-
-        // TODO numPartitions should be configurable for Galapagos metadata topics
-        NewTopic newTopic = new NewTopic(topicName, 3, (short) topicCreateParams.getReplicationFactor());
-        newTopic = newTopic.configs(Map.of(TopicConfig.CLEANUP_POLICY_CONFIG, TopicConfig.CLEANUP_POLICY_COMPACT));
-
-        return toCompletableFuture(this.adminClient.createTopics(Collections.singleton(newTopic)).all()).thenCompose(
-                o -> toCompletableFuture(adminClient.alterConfigs(Collections.singletonMap(res, config)).all()));
+        NewTopic newTopic = new NewTopic(topicName, topicCreateParams.getNumberOfPartitions(),
+                (short) topicCreateParams.getReplicationFactor());
+        newTopic = newTopic.configs(topicCreateParams.getTopicConfigs());
+        return toCompletableFuture(this.adminClient.createTopics(Collections.singleton(newTopic)).all());
     }
 
     @Override
