cd29f26dbcdfab7e11b495aeaf65d6fc87406f23
PolatEmre-hg <71328448+PolatEmre-hg@users.noreply.github.com>
2022-01-11 16:43:26 +0100

    Create new Admin Job "CreateBackup" (#95)

---
+++ src/main/java/com/hermesworld/ais/galapagos/adminjobs/impl/CreateBackupJob.java
@@ -0,0 +1,109 @@
+package com.hermesworld.ais.galapagos.adminjobs.impl;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hermesworld.ais.galapagos.adminjobs.AdminJob;
+import com.hermesworld.ais.galapagos.kafka.KafkaCluster;
+import com.hermesworld.ais.galapagos.kafka.KafkaClusters;
+import com.hermesworld.ais.galapagos.kafka.util.TopicBasedRepository;
+import com.hermesworld.ais.galapagos.util.HasKey;
+import com.hermesworld.ais.galapagos.util.JsonUtil;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.util.Collection;
+import java.util.Optional;
+
+@Component
+public class CreateBackupJob implements AdminJob {
+
+    private final KafkaClusters kafkaClusters;
+
+    private final ObjectMapper objectMapper = JsonUtil.newObjectMapper();
+
+    @Autowired
+    public CreateBackupJob(KafkaClusters kafkaClusters) {
+        this.kafkaClusters = kafkaClusters;
+    }
+
+    @Override
+    public String getJobName() {
+        return "create-backup";
+    }
+
+    @Override
+    public void run(ApplicationArguments allArguments) throws Exception {
+        boolean createBackupFile = Optional.ofNullable(allArguments.getOptionValues("create.backup.file"))
+                .flatMap(ls -> ls.stream().findFirst()).map(Boolean::parseBoolean).orElse(false);
+
+        String outputFileName = Optional.ofNullable(allArguments.getOptionValues("output.filename"))
+                .flatMap(ls -> ls.stream().findFirst()).map(String::new).orElse("backup.json");
+
+        JSONObject backup = new JSONObject();
+
+        System.out.println();
+        System.out.println("========================= Starting Backup Creation ========================");
+        System.out.println();
+
+        kafkaClusters.getEnvironmentIds().forEach(envId -> kafkaClusters.getEnvironment(envId)
+                .ifPresent(env -> backup.put(envId, backupEnvironment(env))));
+
+        System.out.println();
+        System.out.println("========================= Backup Creation COMPLETE ========================");
+        System.out.println();
+
+        if (!createBackupFile) {
+            System.out.println("Backup JSON:");
+            System.out.println();
+            System.out.println(backup.toString(2));
+        }
+
+        if (createBackupFile) {
+            System.out.println("========================= Generating Backup file as json ========================");
+            File file = new File(outputFileName);
+
+            try (Writer writer = new BufferedWriter(new FileWriter(file))) {
+                writer.write(backup.toString(2));
+            }
+            catch (IOException e) {
+                System.err.println("Could not create Backup file");
+                e.printStackTrace();
+                return;
+            }
+
+            System.out.println("========================= Generated Backup file as json in " + outputFileName
+                    + " ========================");
+
+        }
+
+    }
+
+    private JSONObject backupEnvironment(KafkaCluster cluster) {
+        JSONObject result = new JSONObject();
+
+        for (TopicBasedRepository<?> backupTopic : cluster.getRepositories()) {
+            result.put(backupTopic.getTopicName(),
+                    backupTopicData(cluster.getRepository(backupTopic.getTopicName(), backupTopic.getValueClass())));
+        }
+
+        return result;
+    }
+
+    private JSONObject backupTopicData(TopicBasedRepository<? extends HasKey> repo) {
+        JSONObject result = new JSONObject();
+        Collection<? extends HasKey> o = repo.getObjects();
+        for (HasKey obj : o) {
+            try {
+                result.put(obj.key(), new JSONObject(objectMapper.writeValueAsString(obj)));
+            }
+            catch (JSONException | JsonProcessingException e) {
+                e.printStackTrace();
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/com/hermesworld/ais/galapagos/adminjobs/impl/CreateBackupJobTest.java
@@ -0,0 +1,243 @@
+package com.hermesworld.ais.galapagos.adminjobs.impl;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hermesworld.ais.galapagos.applications.ApplicationOwnerRequest;
+import com.hermesworld.ais.galapagos.applications.RequestState;
+import com.hermesworld.ais.galapagos.kafka.KafkaCluster;
+import com.hermesworld.ais.galapagos.kafka.KafkaClusters;
+import com.hermesworld.ais.galapagos.kafka.impl.TopicBasedRepositoryMock;
+import com.hermesworld.ais.galapagos.subscriptions.SubscriptionMetadata;
+import com.hermesworld.ais.galapagos.subscriptions.SubscriptionState;
+import com.hermesworld.ais.galapagos.topics.TopicMetadata;
+import com.hermesworld.ais.galapagos.topics.TopicType;
+import com.hermesworld.ais.galapagos.util.JsonUtil;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.springframework.boot.ApplicationArguments;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class CreateBackupJobTest {
+
+    private KafkaClusters kafkaClusters;
+
+    private final Map<String, TopicBasedRepositoryMock<?>> testRepo = new HashMap<>();
+
+    private final Map<String, TopicBasedRepositoryMock<?>> prodRepo = new HashMap<>();
+
+    private ObjectMapper mapper;
+
+    @Before
+    public void setUp() {
+        testRepo.put("topics", new TopicBasedRepositoryMock<TopicMetadata>() {
+            @Override
+            public Class<TopicMetadata> getValueClass() {
+                return TopicMetadata.class;
+            }
+
+            @Override
+            public String getTopicName() {
+                return "topics";
+            }
+
+            @Override
+            public Collection<TopicMetadata> getObjects() {
+                TopicMetadata meta1 = new TopicMetadata();
+                meta1.setName("topic-1");
+                meta1.setOwnerApplicationId("app-1");
+                meta1.setType(TopicType.EVENTS);
+                return List.of(meta1);
+            }
+
+        });
+        testRepo.put("subscriptions", new TopicBasedRepositoryMock<SubscriptionMetadata>() {
+            @Override
+            public Class<SubscriptionMetadata> getValueClass() {
+                return SubscriptionMetadata.class;
+            }
+
+            @Override
+            public String getTopicName() {
+                return "subscriptions";
+            }
+
+            @Override
+            public Collection<SubscriptionMetadata> getObjects() {
+                SubscriptionMetadata sub1 = new SubscriptionMetadata();
+                sub1.setId("123");
+                sub1.setClientApplicationId("app-1");
+                sub1.setTopicName("topic-1");
+                sub1.setState(SubscriptionState.APPROVED);
+                return List.of(sub1);
+            }
+
+        });
+        testRepo.put("application-owner-requests", new TopicBasedRepositoryMock<ApplicationOwnerRequest>() {
+            @Override
+            public Class<ApplicationOwnerRequest> getValueClass() {
+                return ApplicationOwnerRequest.class;
+            }
+
+            @Override
+            public String getTopicName() {
+                return "application-owner-requests";
+            }
+
+            @Override
+            public Collection<ApplicationOwnerRequest> getObjects() {
+                ApplicationOwnerRequest req = new ApplicationOwnerRequest();
+                req.setApplicationId("app-1");
+                req.setId("1");
+                req.setUserName("myUser");
+                req.setState(RequestState.APPROVED);
+                return List.of(req);
+            }
+
+        });
+
+        prodRepo.put("topics", new TopicBasedRepositoryMock<TopicMetadata>() {
+            @Override
+            public Class<TopicMetadata> getValueClass() {
+                return TopicMetadata.class;
+            }
+
+            @Override
+            public String getTopicName() {
+                return "topics";
+            }
+
+            @Override
+            public Collection<TopicMetadata> getObjects() {
+                TopicMetadata meta1 = new TopicMetadata();
+                meta1.setName("topic-2");
+                meta1.setOwnerApplicationId("app-2");
+                meta1.setType(TopicType.EVENTS);
+                return List.of(meta1);
+            }
+
+        });
+        prodRepo.put("subscriptions", new TopicBasedRepositoryMock<SubscriptionMetadata>() {
+            @Override
+            public Class<SubscriptionMetadata> getValueClass() {
+                return SubscriptionMetadata.class;
+            }
+
+            @Override
+            public String getTopicName() {
+                return "subscriptions";
+            }
+
+            @Override
+            public Collection<SubscriptionMetadata> getObjects() {
+                SubscriptionMetadata sub1 = new SubscriptionMetadata();
+                sub1.setId("12323");
+                sub1.setClientApplicationId("app-12");
+                sub1.setTopicName("topic-2");
+                sub1.setState(SubscriptionState.APPROVED);
+                return List.of(sub1);
+            }
+
+        });
+        prodRepo.put("application-owner-requests", new TopicBasedRepositoryMock<ApplicationOwnerRequest>() {
+            @Override
+            public Class<ApplicationOwnerRequest> getValueClass() {
+                return ApplicationOwnerRequest.class;
+            }
+
+            @Override
+            public String getTopicName() {
+                return "application-owner-requests";
+            }
+
+            @Override
+            public Collection<ApplicationOwnerRequest> getObjects() {
+                ApplicationOwnerRequest req = new ApplicationOwnerRequest();
+                req.setApplicationId("app-2");
+                req.setId("2");
+                req.setUserName("myUser2");
+                req.setState(RequestState.APPROVED);
+                return List.of(req);
+            }
+
+        });
+
+        mapper = JsonUtil.newObjectMapper();
+        kafkaClusters = mock(KafkaClusters.class);
+        KafkaCluster testCluster = mock(KafkaCluster.class);
+        KafkaCluster prodCluster = mock(KafkaCluster.class);
+        when(testCluster.getId()).thenReturn("test");
+        when(prodCluster.getId()).thenReturn("prod");
+        doReturn(testRepo.values()).when(testCluster).getRepositories();
+        doReturn(prodRepo.values()).when(prodCluster).getRepositories();
+        // noinspection SuspiciousMethodCalls
+        when(testCluster.getRepository(any(), any())).then(inv -> testRepo.get(inv.getArgument(0)));
+        // noinspection SuspiciousMethodCalls
+        when(prodCluster.getRepository(any(), any())).then(inv -> prodRepo.get(inv.getArgument(0)));
+        when(kafkaClusters.getEnvironment("test")).thenReturn(Optional.of(testCluster));
+        when(kafkaClusters.getEnvironment("prod")).thenReturn(Optional.of(prodCluster));
+        when(kafkaClusters.getEnvironmentIds()).thenReturn(List.of("test", "prod"));
+    }
+
+    @Test
+    @DisplayName("it should create a backup from all the metadata currently saved within Galapagos")
+    public void createBackUp_success() throws Exception {
+        CreateBackupJob job = new CreateBackupJob(kafkaClusters);
+        ApplicationArguments args = mock(ApplicationArguments.class);
+        when(args.getOptionValues("create.backup.file")).thenReturn(Collections.singletonList("true"));
+
+        try {
+            job.run(args);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        String backUpJson = Files.readString(Path.of("backup.json"));
+
+        JsonNode jsonNode = mapper.readTree(backUpJson);
+        String topicName = jsonNode.get("test").get("topics").get("topic-1").get("name").toString();
+        String topicType = jsonNode.get("test").get("topics").get("topic-1").get("type").toString();
+        String clientApplicationIdSub = jsonNode.get("test").get("subscriptions").get("123").get("clientApplicationId")
+                .toString();
+        String subId = jsonNode.get("test").get("subscriptions").get("123").get("id").toString();
+        String aorId = jsonNode.get("test").get("application-owner-requests").get("1").get("id").toString();
+        String aorState = jsonNode.get("test").get("application-owner-requests").get("1").get("state").toString();
+        String username = jsonNode.get("test").get("application-owner-requests").get("1").get("userName").toString();
+
+        String topicNameProd = jsonNode.get("prod").get("topics").get("topic-2").get("name").toString();
+        String topicTypeProd = jsonNode.get("prod").get("topics").get("topic-2").get("type").toString();
+        String clientApplicationIdSubProd = jsonNode.get("prod").get("subscriptions").get("12323")
+                .get("clientApplicationId").toString();
+        String subIdProd = jsonNode.get("prod").get("subscriptions").get("12323").get("id").toString();
+        String aorIdProd = jsonNode.get("prod").get("application-owner-requests").get("2").get("id").toString();
+        String aorStateProd = jsonNode.get("prod").get("application-owner-requests").get("2").get("state").toString();
+        String usernameProd = jsonNode.get("prod").get("application-owner-requests").get("2").get("userName")
+                .toString();
+
+        // test data
+        assertEquals(topicName, "\"topic-1\"");
+        assertEquals(topicType, "\"EVENTS\"");
+        assertEquals(clientApplicationIdSub, "\"app-1\"");
+        assertEquals(subId, "\"123\"");
+        assertEquals(aorId, "\"1\"");
+        assertEquals(aorState, "\"APPROVED\"");
+        assertEquals(username, "\"myUser\"");
+        // prod data
+        assertEquals(topicNameProd, "\"topic-2\"");
+        assertEquals(topicTypeProd, "\"EVENTS\"");
+        assertEquals(clientApplicationIdSubProd, "\"app-12\"");
+        assertEquals(subIdProd, "\"12323\"");
+        assertEquals(aorIdProd, "\"2\"");
+        assertEquals(aorStateProd, "\"APPROVED\"");
+        assertEquals(usernameProd, "\"myUser2\"");
+
+    }
+
+}
