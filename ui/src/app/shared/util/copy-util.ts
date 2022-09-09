f147e69f0086b9fda0a3794bb89cb0f486e36ce6
PolatEmre-hg <71328448+PolatEmre-hg@users.noreply.github.com>
2022-09-09 17:15:24 +0200

    Use Clipboard API (#218)
    Co-authored-by: Florian Albrecht <53865486+albrechtflo-hg@users.noreply.github.com>

---
+++ ui/src/app/shared/util/copy-util.ts
@@ -0,0 +1,32 @@
+import { Observable } from 'rxjs';
+
+export const copy = (value: string) => {
+    const selBox = createTextArea();
+    selBox.value = value;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    if (navigator.clipboard) {
+        navigator.clipboard.writeText(value);
+    } else {
+        // noinspection JSDeprecatedSymbols
+        document.execCommand('copy');
+    }
+    document.body.removeChild(selBox);
+};
+
+export const copyObsValue = (observer: Observable<string>) => {
+    const subscription = observer.subscribe(value => {
+        copy(value);
+        subscription.unsubscribe();
+    });
+};
+
+const createTextArea = (): HTMLTextAreaElement => {
+    const textarea = document.createElement('textarea');
+    textarea.style.position = 'fixed';
+    textarea.style.left = '0';
+    textarea.style.top = '0';
+    textarea.style.opacity = '0';
+    return textarea;
+};
