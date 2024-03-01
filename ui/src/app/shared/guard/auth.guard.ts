db2b170888e7ef79947c1ac8ed93f1259b8e370d
Florian Albrecht <53865486+albrechtflo-hg@users.noreply.github.com>
2024-03-01 17:34:10 +0100

    Upgrade to Angular 16 (unusable due to ngx-slider)

--- ui/src/app/shared/guard/auth.guard.ts
+++ ui/src/app/shared/guard/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { firstValueFrom, map } from 'rxjs';
 
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
 
     constructor(private router: Router, private authService: AuthService) {
     }
